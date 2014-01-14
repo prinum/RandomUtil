3a11b7f13f390ad414b745702b35f47fc2ea3cb8
daichi <prinum101@gmail.com>
2014-01-14 17:00:32 +0900

    use Array.sample

--- lib/random_util.rb
+++ lib/random_util.rb
@@ -2,19 +2,19 @@ require "random_util/version"
 
 module RandomUtil
   ALPHABETS = ('a'..'z').to_a + ('A'..'Z').to_a
-  NUMBERS = (0..9).map{|n| n}
+  NUMBERS = (0..9).to_a
   
   def self.alphabets(counts)
-    Array.new(counts).map { ALPHABETS[rand(ALPHABETS.length)] }.join
+    Array.new(counts).map { ALPHABETS.sample }.join
   end
 
   def self.numbers(counts)
-    Array.new(counts).map { NUMBERS[rand(NUMBERS.length)] }.join
+    Array.new(counts).map { NUMBERS.sample] }.join
   end
 
   def self.alphabet_and_number(counts)
     symbols = ALPHABETS + NUMBERS
-    Array.new(counts).map { symbols[rand(symbols.length)] }.join
+    Array.new(counts).map { symbols.sample }.join
   end
   
 end
--- lib/random_util/version.rb
+++ lib/random_util/version.rb
@@ -1,3 +1,3 @@
 module RandomUtil
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
